f2f4d21447b8e4c776794b02b76b501749e022f7
Chetan2028 <bmchetan2028@gmail.com>
2020-10-22 00:10:16 +0530

    [Chetan] Add . Added Program file to Check equality of lines

--- Program.cs
+++ Program.cs
@@ -6,19 +6,6 @@ namespace LineComparison
     {
 
         /// <summary>
-        /// To Calculate the Length the of the line.
-        /// </summary>
-        /// <param name="x1">The x1.</param>
-        /// <param name="y1">The y1.</param>
-        /// <param name="x2">The x2.</param>
-        /// <param name="y2">The y2.</param>
-        public void LengthOfTheLine(int x1, int y1, int x2, int y2)
-        {
-            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("Length of the line : " + length);
-        }
-
-        /// <summary>
         /// Checkings the equalityof lines.
         /// </summary>
         /// <param name="x1">The x1.</param>
@@ -29,10 +16,8 @@ namespace LineComparison
         /// <param name="y3">The y3.</param>
         /// <param name="x4">The x4.</param>
         /// <param name="y4">The y4.</param>
-        public void CheckingEqualityofLines(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+        public void CheckingEqualityofLines(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4,double length1 , double length2)
         {
-            double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            double length2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
             if(length1 == length2)
             {
                 Console.WriteLine("Line1 and Line2 are equal");
@@ -47,7 +32,9 @@ namespace LineComparison
         /// </summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
-        {
+        { ///Create a reference of a class
+            Program program = new Program();
+
             Console.WriteLine("Enter the Co-ordinates for x1 and y1");
             int x1 = Convert.ToInt32(Console.ReadLine());
             int y1 = Convert.ToInt32(Console.ReadLine());
@@ -55,8 +42,9 @@ namespace LineComparison
             int x2 = Convert.ToInt32(Console.ReadLine());
             int y2 = Convert.ToInt32(Console.ReadLine());
 
-            Program program = new Program();
-            program.LengthOfTheLine(x1, y1, x2, y2);
+            ///To find the length of the line1
+            double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("The length of line1 is : " + length1);
 
             Console.WriteLine("Enter the Co-ordinates for x3 and y3");
             int x3 = Convert.ToInt32(Console.ReadLine());
@@ -65,7 +53,11 @@ namespace LineComparison
             int x4 = Convert.ToInt32(Console.ReadLine());
             int y4 = Convert.ToInt32(Console.ReadLine());
 
-            program.CheckingEqualityofLines(x1, y1, x2, y2, x3, y3, x4, y4);
+            ///To find the length of the line2
+            double length2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+            Console.WriteLine("The length of line2 is : " + length2);
+            ///Checking equality of lines using Equals method
+            program.CheckingEqualityofLines(x1, y1, x2, y2, x3, y3, x4, y4, length1, length2);
         }
     }
 }
