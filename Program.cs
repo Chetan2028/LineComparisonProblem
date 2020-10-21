6182a7f4b0343a4a15e46bd4cd302b270e9ba2fe
Chetan2028 <bmchetan2028@gmail.com>
2020-10-22 00:15:25 +0530

    [Chetan] Add . Added Program file to compute length

--- Program.cs
+++ Program.cs
@@ -4,22 +4,12 @@ namespace LineComparison
 {
     class Program
     {
-        ///Variable
-        double length = 0;
-        /// <summary>
-        /// To Calculate the Length the of the line.
-        /// </summary>
-        /// <param name="x1">The x1.</param>
-        /// <param name="y1">The y1.</param>
-        /// <param name="x2">The x2.</param>
-        /// <param name="y2">The y2.</param>
-        public void LengthOfTheLine(int x1 , int y1, int x2, int y2)
-        {
-            length = Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2));
-            Console.WriteLine("Length of the line : " + length);
-        }
+        
         static void Main(string[] args)
         {
+            ///Create a reference of a class
+            Program program = new Program();
+
             Console.WriteLine("Enter the Co-ordinates for x1 and y1");
             int x1 = Convert.ToInt32(Console.ReadLine());
             int y1 = Convert.ToInt32(Console.ReadLine());
@@ -27,8 +17,9 @@ namespace LineComparison
             int x2 = Convert.ToInt32(Console.ReadLine());
             int y2 = Convert.ToInt32(Console.ReadLine());
 
-            Program program = new Program();
-            program.LengthOfTheLine(x1,y1,x2,y2);
+            ///To find the length of the line
+            double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("The length of line1 is : " + length1);
         }
     }
 }
